ff21cc6654691d292bd307c47457b518bf19f79e
shubham54 <shubhamchauhan54@gmail.com>
2016-06-19 11:41:24 +0530

    Error Corrected

--- Coin Changing Minimum Number of Coins.cpp
+++ Coin Changing Minimum Number of Coins.cpp
@@ -11,9 +11,10 @@ int main()
         cin>>W[i];
     sort(W,W+n);
     int dp[n][sum+1];
-    for(int i=0;i<=sum;i++)
+    dp[0][0]=0;
+    for(int i=1;i<=sum;i++)
     {
-        if(W[0]>i)
+        if(i%W[0])
             dp[0][i]=0;
         else
             dp[0][i]=1+dp[0][i-W[0]];
@@ -28,7 +29,7 @@ int main()
                 dp[i][j]=min(dp[i][j-W[i]]+1,dp[i-1][j]);
         }
     }
-    cout<<"No. Of Coin Required="<<dp[n-1][sum]<<endl;
+    cout<<"Min No. Of Coin Required="<<dp[n-1][sum]<<endl;
     //Matrix
     for(int i=0;i<n;i++)
     {
