b4cce5aad6d537e7ec258825e561116170653f79
shubham54 <shubhamchauhan54@gmail.com>
2016-06-26 17:06:59 +0530

    third commit

---
+++ Count Number of Binary Search Tree Possible given n keys.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int dp[n+1];
+    dp[1]=1;dp[2]=2;
+    for(int i=3;i<=n;i++){
+        dp[i]=2*dp[i-1];
+        for(int j=2;j<i;j++){
+            dp[i]+=dp[j-1]*dp[i-j];
+        }
+    }
+    cout<<"Answer is= "<<dp[n]<<endl;
+    return 0;
+}
